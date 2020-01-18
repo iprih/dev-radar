f998b71954cdafc11579e1289064015bc7188874
iprih <priscila.silva076@gmail.com>
2020-01-18 03:08:42 -0300

    consumindo api github

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,15 @@
 const { Router } = require('express');
-
+const axios = require('axios')
 const routes = Router();
 
 
-routes.post('/devs', (req, res) => {
-    console.log(request.body);
+routes.post('/devs', async (req, res) => {
+    const { github_username } = req.body;
+
+    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
+
+    console.log(apiResponse.data)
+    
     return res.json({ message: 'Hello oministack'})
 
 });
